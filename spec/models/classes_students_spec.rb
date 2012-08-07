2117a87a2a8ecd6718ea6cd802f4beafde5957a2
Apprentice <apprentice@devbootcamp.com>
2012-08-07 14:44:44 -0700

    wasted time redeleting already deleted files

--- spec/models/classes_students_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe ClassesStudents do
-  it {should belong_to :student}
-  it {should belong_to :day_class}
-end
--- spec/models/day_class_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe DayClass do
-  it { should have_many :students}
-  it { should belong_to :teacher}
-  it { should belong_to :grade_level }
-  it { should belong_to :subject}
-  it { should have_many :assignments}
-end
