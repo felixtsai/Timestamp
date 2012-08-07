e2e87ea52bd965614bf56511fb95bb078efe8ccc
dbc-apprentice <apprentice@devbootcamp.com>
2012-08-06 21:28:11 -0700

    fix simplecov

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120807023705) do
+ActiveRecord::Schema.define(:version => 20120807032823) do
 
   create_table "afterschool_classes", :force => true do |t|
     t.integer  "grade_level_id"
