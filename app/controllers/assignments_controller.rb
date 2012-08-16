0ccab9f1fe61a18c6df000e4e3f7b0af69a59e83
Apprentice <apprentice@devbootcamp.com>
2012-08-16 10:48:06 -0700

    committed

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -9,7 +9,7 @@ class AssignmentsController < ApplicationController
       @assignments = Assignment.all
     end
     respond_to do |format|
-      format.csv { send_data @assignments.to_csv}
+      # format.csv { send_data @assignments.to_csv}
       format.xls # { send_data @assignments.to_csv(col_sep: "\t") }
       format.html do
         if request.xhr?
